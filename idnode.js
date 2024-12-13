56a4fbc04bc79b6d696013c78aaeb6dbb4afd587
Furqonflynn <69003304+cmalf@users.noreply.github.com>
2024-12-14 04:43:44 +0700

    Adding Source Code
    - bless.js
    - config.js
    - idnode.js
    - package.json
    - proxy.txt

---
+++ idnode.js
@@ -0,0 +1,9 @@
+// Node IDs
+export const NODE_IDS = [
+    "12D3KooxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxqgeUjZmKNW3Nt",  // Node 1
+    "12D3KooxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxqgeUjZmKNW3Nt",  // Node 2 etc
+    "",
+    "",
+    "",
+    "",
+];
