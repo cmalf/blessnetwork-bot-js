2abb49ec08c1c2d894809478eff0abbaf321b4ec
Furqonflynn <69003304+cmalf@users.noreply.github.com>
2024-12-21 15:18:51 +0700

    Update fixed idnode.js

--- idnode.js
+++ idnode.js
@@ -1,9 +1,12 @@
-// Node IDs
-export const NODE_IDS = [
-    "12D3KooxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxqgeUjZmKNW3Nt",  // Node 1
-    "12D3KooxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxqgeUjZmKNW3Nt",  // Node 2 etc
-    "",
-    "",
-    "",
-    "",
+// idnode.js
+const NODE_IDS = [
+    "node_id_1",
+    "node_id_2",
+    "node_id_3"
+    // Add more node IDs as needed
 ];
+
+module.exports = {
+    NODE_IDS
+};
+
