f8c88ac64cad70292be8e936e79cfa4a0e17918c
Jagocuan <caturalfurqon.4@gmail.com>
2025-01-20 03:30:16 +0700

    Adding MultiAccounts Support

--- config.js
+++ config.js
@@ -1,10 +1,47 @@
 //configuration
 
-const set = {
-	AUTH_TOKEN: '',  // Replace with your Bearer token
-	UserAgent: '', // Replace with your UserAgent
-	proxyFile: 'proxy.txt', // your Path to Proxy.txt file
+const accounts = [
+    {
+        AUTH_TOKEN: 'xxx', // Bearer token for Account 1
+    },
+    {
+        AUTH_TOKEN: 'xxx', // Bearer token for Account 2
+    },
+    // Add more accounts as needed
+];
 
-	};
+const USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Edge/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Edge/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Edge/120.0.0.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 OPR/114.0.0.",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 AtContent/95.5.5462.5",
+];
 
-module.exports = set;
+/**
+ * Returns a random User-Agent string from the USER_AGENTS array.
+ * @returns {string} A random User-Agent string.
+ * @throws {Error} If the USER_AGENTS array is empty.
+ */
+function getRandomUserAgent() {
+    if (USER_AGENTS.length === 0) {
+        throw new Error('User agents list is empty.');
+    }
+    const index = Math.floor(Math.random() * USER_AGENTS.length);
+    return USER_AGENTS[index];
+}
+
+const proxyFile = 'proxy.txt'; // Path to your proxy.txt file
+
+module.exports = {
+    accounts,
+    getRandomUserAgent,
+    proxyFile,
+};
